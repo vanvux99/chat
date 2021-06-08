760460115b72e0aabdbc5720bf1c885dd71151f0
vũ <vanvux99@gmail.com>
2021-06-08 22:05:35 +0700

    create page infomation

---
+++ chatbox/components/Infomation/info.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-header">Tài Khoản Của Tôi</div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm-1">
+            <!-- <div class="nenxanh">Cột thứ 1</div>
+            <div>
+                <img class="card-img-top" src="" alt="this is avt">
+            </div> -->
+          </div>
+          <div class="col-sm-3">
+            <!-- <div class="nenxanh">Cột thứ 1</div> -->
+            <div>
+              <img class="card-img-top" src="" alt="this is avt" />
+            </div>
+          </div>
+          <div class="col-sm-7">
+            <!-- <div class="nenhong">Cột thứ 2</div> -->
+            <div>
+              <h5 class="card-title">this is name</h5>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-3"></div>
+          <div class="col-sm-5">
+            <!-- <div class="nenhong">Cột thứ 2</div> -->
+            <div>
+              <input
+                type="text"
+                readonly
+                class="form-control-plaintext"
+                id="staticEmail"
+                value="email@example.com"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ chatbox/pages/Info/info.vue
@@ -0,0 +1,17 @@
+<template>
+  <INFO />
+</template>
+
+<script>
+import INFO from '../../components/Infomation/info.vue'
+export default {
+  layout: 'box',
+  components: {
+    INFO
+  }
+}
+</script>
+
+<style>
+
+</style>
